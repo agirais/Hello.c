2c48d8cdc7edb1036d36c96afc2d8d1779639853
agirais <agirais.who-am-i.freedom@docomo.co.jp>
2014-05-29 01:28:54 +0900

    Hello10

---
+++ Hello10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+	printf("Hello World\n");
+		
+	return 0;
+
+	
+}
